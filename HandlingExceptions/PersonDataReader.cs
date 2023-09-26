8eca8a1b0a32ec69478d8035829547ae1505414d
Iggy988 <igor.rodic1988@gmail.com>
2023-09-26 21:55:00 +0200

    Adding Exception filters

---
+++ HandlingExceptions/PersonDataReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions;
+
+public class PersonDataReader
+{
+    private readonly IPeopleRepository _peopleRepository;
+    private readonly ILogger _logger;
+
+    public PersonDataReader(
+        IPeopleRepository personRepository,
+        ILogger logger)
+    {
+        _peopleRepository = personRepository;
+        _logger = logger;
+    }
+
+    public Person ReadPersonData(int personId)
+    {
+        try
+        {
+            return _peopleRepository.Read(personId);
+        }
+        catch (Exception ex)
+        {
+            _logger.Log(ex);
+            throw;
+        }
+    }
+}
+
+public interface IPeopleRepository
+{
+    Person Read(int personId);
+}
+
+public interface ILogger
+{
+    void Log(Exception ex);
+}
--- HandlingExceptions/Program.cs
+++ HandlingExceptions/Program.cs
@@ -89,16 +89,53 @@ bool IsFirstElementPositive(IEnumerable<int> numbers)
         var firstElement = GetFirstElement(numbers);
         return firstElement > 0;
     }
-    catch(InvalidOperationException ex) 
+    catch (InvalidOperationException ex)
     {
         Console.WriteLine("The collection is empty!");
-        return true; 
+        return true;
     }
     catch (NullReferenceException ex)
     {
-        throw new ArgumentNullException("The collection is null.", ex);    
+        Console.WriteLine("Sorry! The application experienced " +
+            "an unexpected error.");
+        //alternatively, we can just rethrow the original exception:
+        //throw;
+        throw new ArgumentNullException("The collection is null.", ex);
     }
 }
+
+//Exception filters
+try
+{
+    var dataFromWeb = SendHttpRequest("www.someAddress.com/get/someResource");
+}
+catch (HttpRequestException ex) when (ex.Message == "403")
+{
+    Console.WriteLine("It was forbidden to access the resource.");
+    throw;
+}
+catch (HttpRequestException ex) when (ex.Message == "404")
+{
+    Console.WriteLine("The resource was not found.");
+    throw;
+}
+catch (HttpRequestException ex) when (ex.Message.StartsWith("4"))
+{
+    Console.WriteLine("Some kind of client error.");
+    throw;
+}
+catch (HttpRequestException ex) when (ex.Message == "500")
+{
+    Console.WriteLine("The server has experienced an internal error.");
+    throw;
+}
+
+object SendHttpRequest(string url)
+{
+    //send the request
+    return null;
+}
+
 
 
 
