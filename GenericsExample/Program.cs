d2b42c2e7ab63ac4f9dfa63447a54474273e8b4b
Iggy988 <igor.rodic1988@gmail.com>
2023-10-01 18:17:08 +0200

    Adding Generic method

--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections;
 //By using generic types, we can define the behavior of a type(List etc.),
 //once and reuse it for multiple types, reducing the amount of code we need to write
 
@@ -34,14 +34,38 @@
 
 
 var numbers = new List<int> { 1, 2, 3, 4, 5 };
-Tuple<int, int> minAndMax = GetMinAndMax(numbers);
+var ints = new List<int> { 1, 2, 3};
 
-var twoStrings = new Tuple<string, string>("ime", "prezime");
-var differentTuple = new SimpleTuple<string, int>("godina", 2);
-var threeItems = new SimpleTuple<string, int, bool>("godina", 2, false);
+ints.AddToFront(10);
+ints.AddToFront<int>(11);
+//ints.AddToFront<int>("string");
+//Tuple<int, int> minAndMax = GetMinAndMax(numbers);
 
-Console.WriteLine("Smallest number: " + minAndMax.Item1);
-Console.WriteLine("Largest number: " + minAndMax.Item2);
+//var twoStrings = new Tuple<string, string>("ime", "prezime");
+//var differentTuple = new SimpleTuple<string, int>("godina", 2);
+//var threeItems = new SimpleTuple<string, int, bool>("godina", 2, false);
+
+//Console.WriteLine("Smallest number: " + minAndMax.Item1);
+//Console.WriteLine("Largest number: " + minAndMax.Item2);
+
+
+
+ArrayList ints2 = new ArrayList { 2, 3, 4, 5, 6};
+ArrayList strings = new ArrayList { "a", "b", "c", "d" };
+
+ArrayList variousItems = new ArrayList { 1, "abc", false, new DateTime()};
+object[] objects = new object[] { 1, "abc", false, new DateTime() };
+
+int sum = 0;
+
+foreach (var number in ints)
+{
+    if (number is int)
+    {
+        sum += (int)number;
+    }
+    
+} 
 
 Console.ReadKey();
 
@@ -69,28 +93,10 @@ Tuple<int, int> GetMinAndMax(IEnumerable<int> input)
     return new Tuple<int, int>(min, max);
 }
 
-public class SimpleTuple<T1, T2>
+static class ListExtensions
 {
-    public SimpleTuple(T1 item1, T2 item2)
+    public static void AddToFront<T>(this List<T> list, T value)
     {
-        Item1 = item1;
-        Item2 = item2;
+        list.Insert(0, value);
     }
-
-    public T1 Item1 { get; }
-    public T2 Item2 { get; }
-}
-
-public class SimpleTuple<T1, T2, T3>
-{
-    public SimpleTuple(T1 item1, T2 item2, T3 item3)
-    {
-        Item1 = item1;
-        Item2 = item2;
-        Item3 = item3;
-    }
-
-    public T1 Item1 { get; }
-    public T2 Item2 { get; }
-    public T3 Item3 { get; }
 }
---
+++ GenericsExample/SimpleTuple1.cs
@@ -0,0 +1,50 @@
+﻿
+//By using generic types, we can define the behavior of a type(List etc.),
+//once and reuse it for multiple types, reducing the amount of code we need to write
+
+//Data structures are types meant for storing and orginizing data so that it can be accessed and modified efficiently
+// arrays, stacks, queues, dictionaries..,
+
+//Algoritm is a set of steps or instructions that are followed in ordder to solve a problem
+
+//Tuple represent set of value
+
+
+//var words = new List<string> { "ime", "prezime", "godina"};
+//var dates = new List<DateTime> { new DateTime(day: 12, month: 3, year: 2023)};
+
+//var numbers = new SimpleList<int>();
+//numbers.Add(1);
+//numbers.Add(2);
+//numbers.Add(3);
+//numbers.Add(4);
+//numbers.Add(5);
+
+//numbers.RemoveAt(2);
+
+//var words = new SimpleList<string>();
+//words.Add("aaa");
+//words.Add("bbb");
+//words.Add("ccc");
+
+//var dates = new SimpleList<DateTime>();
+//dates.Add(new DateTime(day: 12, month: 3, year: 2023));
+//dates.Add(new DateTime(day: 21, month: 5, year: 2018));
+//dates.Add(new DateTime(day: 5, month: 1, year: 1996));
+
+
+
+
+
+
+public class SimpleTuple<T1, T2>
+{
+    public SimpleTuple(T1 item1, T2 item2)
+    {
+        Item1 = item1;
+        Item2 = item2;
+    }
+
+    public T1 Item1 { get; }
+    public T2 Item2 { get; }
+}
