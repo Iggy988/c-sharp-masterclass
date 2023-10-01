eb6cc79eab891e10004cc63ddc33078ab66393fb
Iggy988 <igor.rodic1988@gmail.com>
2023-10-01 09:16:08 +0200

    Creating new Project Generics

---
+++ GenericsExample/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//By using generic types, we can define the behavior of a type(List etc.),
+//once and reuse it for multiple types, reducing the amount of code we need to write
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+var words = new List<string> { "ime", "prezime", "godina"};
+var dates = new List<DateTime> { new DateTime(day: 12, month: 3, year: 2023)};
+
+Console.ReadKey();
