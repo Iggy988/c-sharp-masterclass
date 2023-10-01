98a3ed8608ae8f3cd92d418dfa4466556353490e
Iggy988 <igor.rodic1988@gmail.com>
2023-10-01 09:46:19 +0200

    Creating method for storing int to array

--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -1,8 +1,76 @@
 ﻿
 //By using generic types, we can define the behavior of a type(List etc.),
 //once and reuse it for multiple types, reducing the amount of code we need to write
-var numbers = new List<int> { 1, 2, 3, 4, 5 };
-var words = new List<string> { "ime", "prezime", "godina"};
-var dates = new List<DateTime> { new DateTime(day: 12, month: 3, year: 2023)};
+
+//Data structures are types meant for storing and orginizing data so that it can be accessed and modified efficiently
+// arrays, stacks, queues, dictionaries..,
+
+//var numbers = new List<int> { 1, 2, 3, 4, 5 };
+//var words = new List<string> { "ime", "prezime", "godina"};
+//var dates = new List<DateTime> { new DateTime(day: 12, month: 3, year: 2023)};
+
+var numbers = new ListOfInts();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+
+numbers.RemoveAt(2);
+
+
 
 Console.ReadKey();
+
+
+class ListOfInts
+{
+    private int[] _items = new int[4];
+    private int _size = 0;
+
+    public void Add(int item)
+    {
+        //check is array size is larger or equal arrays length
+        if (_size >= _items.Length)
+        {
+            //if so, we create new array, snd double the size of old one
+            var newItems = new int[_items.Length * 2];
+            //iterating old array and copy its elements one by one to new array
+            for (int i = 0; i < _items.Length; ++i)
+            {
+                newItems[i] = _items[i];
+            }
+            //replace old with new array
+            _items = newItems;
+        }
+
+        _items[_size] = item;
+        ++_size;
+    }
+
+    //remove item by given index
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _size)
+        {
+            throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+        }
+        --_size;
+
+        for (int i = index; i < _size; ++i)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _items[_size] = 0;
+    }
+
+    public int GetAtIndex(int index)
+    {
+        if (index < 0 || index >= _size)
+        {
+            throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+        }
+        return _items[index];
+    }
+}
