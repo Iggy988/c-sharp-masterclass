7c030852b8ca119345569a4e83aea3d9a989e3b7
Iggy988 <igor.rodic1988@gmail.com>
2023-10-06 23:04:43 +0200

    Any

---
+++ LINQExample/Data/PetType.cs
@@ -0,0 +1,7 @@
+﻿public enum PetType
+{
+    Cat,
+    Dog,
+    Fish
+}
+
--- LINQExample/Program.cs
+++ LINQExample/Program.cs
@@ -63,6 +63,29 @@ Console.WriteLine(string.Join(Environment.NewLine, animalsWithD));
 //    Console.WriteLine(animal);
 //}
 
+var pets = new[]
+            {
+                new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+                new Pet(2, "Anthony", PetType.Cat, 2f),
+                new Pet(3, "Ed", PetType.Cat, 0.7f),
+                new Pet(4, "Taiga", PetType.Dog, 35f),
+                new Pet(5, "Rex", PetType.Dog, 40f),
+                new Pet(6, "Lucky", PetType.Dog, 5f),
+                new Pet(7, "Storm", PetType.Cat, 0.9f),
+                new Pet(8, "Nyan", PetType.Cat, 2.2f)
+            };
+
+var isAnyPetNamedBruce = pets.Any(pet => pet.Name == "Bruce");
+Printer.Print(isAnyPetNamedBruce, nameof(isAnyPetNamedBruce));
+
+var isAnyFish = pets.Any(pet => pet.PetType == PetType.Fish);
+Printer.Print(isAnyFish, nameof(isAnyFish));
+
+var isThereAVerySpecifcPet= pets.Any(pet => pet.Name.Length > 6 && pet.Id % 2 == 0);
+Printer.Print(isThereAVerySpecifcPet, nameof(isThereAVerySpecifcPet));
+
+var isNotEmpty = pets.Any();
+
 Console.ReadKey();
 
 static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
@@ -89,7 +112,4 @@ static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
     }
     return false;
 }
-
-
-
 
