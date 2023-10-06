6ac4dfec2d9c8df5d5876546ffa29b54f0e0369f
Iggy988 <igor.rodic1988@gmail.com>
2023-10-06 22:04:48 +0200

    Add Linq Project Example

---
+++ LINQExample/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+
+var wordsNoUppercase = new string[]
+{
+    "quick", "brown", "fox"
+};
+Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
+var wordsWithUppercase = new string[]
+{
+    "quick", "brown", "FOX"
+};
+Console.WriteLine(IsAnyWordUpperCase_Linq(wordsWithUppercase));
+
+Console.WriteLine(orderWords);
+
+Console.ReadKey();
+
+static bool IsAnyWordUpperCase_Linq(IEnumerable<string> words)
+{ 
+    return words.Any(word => word.All(letter =>char.IsUpper(letter)));
+}
+
+    static bool IsAnyWordUpperCase(IEnumerable<string> words)
+{
+    foreach (var word in words)
+    {
+        bool areUpperCase = true;  
+        foreach (var letter in word) 
+        {
+            if (char.IsLower(letter))
+            {
+                areUpperCase = false;
+            }
+        }
+        if (areUpperCase)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
