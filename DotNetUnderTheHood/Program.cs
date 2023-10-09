346a52f5737f390ff00cf4761fe3cd587ad959ff
Iggy988 <igor.rodic1988@gmail.com>
2023-10-09 22:23:54 +0200

    Difference between Value Types and Reference Types

--- DotNetUnderTheHood/Program.cs
+++ DotNetUnderTheHood/Program.cs
@@ -7,7 +7,12 @@ Console.WriteLine("AnotherNumber is " + anotherNumber);
 
 var john = new Person { Name ="John", Age= 34};
 var person = john;
-person.Age = 36;
+//person.Age = 36;
+
+AddOneToNumber(number);
+AddOneToPersonAge(john);
+Console.WriteLine("Number now is " + number);
+Console.WriteLine("Johns age now is " + john.Age);
 
 //classes has references semantics
 // 2 variables can hold a reference to the same object
@@ -15,6 +20,16 @@ Console.WriteLine("Johns age is " + john.Age);
 Console.WriteLine("Persons age is " + person.Age);
 
 Console.ReadKey();
+
+void AddOneToNumber(int number)
+{
+    ++number;
+}
+
+void AddOneToPersonAge(Person person)
+{
+    ++person.Age;
+}
 
 class Person
 {
