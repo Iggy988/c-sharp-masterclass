56c0084e9f2f51473397c61bdb8a615f3095eb3a
Iggy988 <igor.rodic1988@gmail.com>
2023-10-09 22:11:56 +0200

    Dot Net under th hood

---
+++ DotNetUnderTheHood/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = 5;
+int anotherNumber = number;
+anotherNumber++;
+
+Console.WriteLine("Number is " + number);
+Console.WriteLine("AnotherNumber is " + anotherNumber);
+
+var john = new Person { Name ="John", Age= 34};
+var person = john;
+person.Age = 36;
+
+//classes has references semantics
+// 2 variables can hold a reference to the same object
+Console.WriteLine("Johns age is " + john.Age);
+Console.WriteLine("Persons age is " + person.Age);
+
+Console.ReadKey();
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
