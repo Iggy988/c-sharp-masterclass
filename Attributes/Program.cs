ee65b8b7441286c9719049c90be2082df36544aa
Iggy988 <igor.rodic1988@gmail.com>
2023-10-15 19:44:59 +0200

    Structs

--- Attributes/Program.cs
+++ Attributes/Program.cs
@@ -44,39 +44,6 @@ class StringLengthValidate: Attribute
     }
 }
 
-class Validator
-{
-    public bool Validate(object obj)
-    {
-        var type = obj.GetType();
-        var propertiesToValidate = type.GetProperties()
-            // select those properties of this type for which is this attribute defined
-            .Where(prop => Attribute.IsDefined(prop, typeof(StringLengthValidate)));
-
-        foreach (var prop in propertiesToValidate)
-        {
-            object? propertyValue = prop.GetValue(obj);
-            // prvo provjeravamo da li je properyValue string
-            if (propertyValue is not string)
-            {
-                throw new InvalidOperationException($"Attribute {nameof(StringLengthValidate)} can only be applied to strings");
-            }
-
-            // validate
-            var value = (string)propertyValue; // cast property to string, it is safe, we checked lines before
-            var attribute = (StringLengthValidate)prop.GetCustomAttributes(typeof(StringLengthValidate), true).First();
-            if (value.Length < attribute.Min || value.Length > attribute.Max)
-            {
-                Console.WriteLine($"Property {prop.Name} is ivalid, value is {value}");
-                return false;
-            }
-        }
-
-        return true;
-        
-    }
-}
-
 [SomeAttribute(new int[] { 1,2,3})] // ne moze class
 public class SomeClass
 {
---
+++ StructsExample/Program.cs
@@ -0,0 +1,41 @@
+﻿
+var point = new Point(1, 3);
+
+var anotherPoint = point;
+anotherPoint.Y = 100;
+
+Console.WriteLine(point);
+Console.WriteLine(anotherPoint);
+
+SomeMethod(5); //value type
+//SomeMethod(new Person()); // ne moze reference type
+
+Console.ReadKey();
+
+void SomeMethod<T>(T param) where T: struct
+{
+
+}
+
+struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString()
+    {
+        return "X: "+ X + ", Y: " + Y;
+    }
+}
+
+class Person
+{
+    public int Id { get; init; }
+    public string Name { get; init; }    
+}
