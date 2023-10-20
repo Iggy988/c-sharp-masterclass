dbd41760d095ef356dfee1b73817ff99654a49d7
Iggy988 <igor.rodic1988@gmail.com>
2023-10-20 22:57:57 +0200

    Catching data from api

--- API_Example/Program.cs
+++ API_Example/Program.cs
@@ -1,3 +1,70 @@
 ﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var baseAddress = "https://datausa.io/api/";
+var requestUri = "data?drilldowns=Nation&measures=Population";
+
+IApiDataReader apiDataReader = new ApiDataReader();
+var json = await apiDataReader.Read(baseAddress, requestUri);
+
+var root = JsonSerializer.Deserialize<Root>(json);
+
+foreach (var item in root.data)
+{
+    Console.WriteLine($"Year: {item.Year}, population {item.Population}");
+}
 
 Console.ReadKey();
+
+public interface IApiDataReader
+{
+    Task<string> Read(string baseAddress, string requestUri);
+}
+
+public class ApiDataReader : IApiDataReader
+{
+    public async Task<string> Read(string baseAddress, string requestUri)
+    {
+        using var client = new HttpClient();
+        client.BaseAddress = new Uri(baseAddress);
+        HttpResponseMessage response = await client.GetAsync(requestUri);
+
+        //check if it posible to catch data
+        response.EnsureSuccessStatusCode();
+
+        //extract json string from response
+        return await response.Content.ReadAsStringAsync();
+    }
+}
+
+public record Annotations(
+      [property: JsonPropertyName("source_name")] string source_name,
+      [property: JsonPropertyName("source_description")] string source_description,
+      [property: JsonPropertyName("dataset_name")] string dataset_name,
+      [property: JsonPropertyName("dataset_link")] string dataset_link,
+      [property: JsonPropertyName("table_id")] string table_id,
+      [property: JsonPropertyName("topic")] string topic,
+      [property: JsonPropertyName("subtopic")] string subtopic
+  );
+
+public record Datum(
+    [property: JsonPropertyName("ID Nation")] string IDNation,
+    [property: JsonPropertyName("Nation")] string Nation,
+    [property: JsonPropertyName("ID Year")] int IDYear,
+    [property: JsonPropertyName("Year")] string Year,
+    [property: JsonPropertyName("Population")] int Population,
+    [property: JsonPropertyName("Slug Nation")] string SlugNation
+);
+
+public record Root(
+    [property: JsonPropertyName("data")] IReadOnlyList<Datum> data,
+    [property: JsonPropertyName("source")] IReadOnlyList<Source> source
+);
+
+public record Source(
+    [property: JsonPropertyName("measures")] IReadOnlyList<string> measures,
+    [property: JsonPropertyName("annotations")] Annotations annotations,
+    [property: JsonPropertyName("name")] string name,
+    [property: JsonPropertyName("substitutions")] IReadOnlyList<object> substitutions
+);
