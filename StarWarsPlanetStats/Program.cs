ef665e06b2cb9fd7fac8f7a9acb4070d93c40970
Iggy988 <igor.rodic1988@gmail.com>
2023-10-21 22:06:50 +0200

    Exception handling

--- StarWarsPlanetStats/Program.cs
+++ StarWarsPlanetStats/Program.cs
@@ -2,10 +2,54 @@
 using StarWarsPlanetStats.DTO;
 using System.Text.Json;
 
-IApiDataReader apiDataReader = new ApiDataReader();
-var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
-
-var root = JsonSerializer.Deserialize<Result>(json);
+try
+{
+    await new StarWarsPlanetsStatsApp(
+        new ApiDataReader(),
+        new MockStarWarsApiDataReader()).Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred. Exception message: " + ex.Message);
+}
 
 Console.WriteLine("Press any key to close");
 Console.ReadKey();
+
+
+internal class StarWarsPlanetsStatsApp
+{
+    //Dependency inversion - classes should depend on abstraction
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondaryApiDataReader;
+
+    public StarWarsPlanetsStatsApp(IApiDataReader apiDataReader, IApiDataReader secondaryApiDataReader)
+    {
+        _apiDataReader = apiDataReader;
+        _secondaryApiDataReader = secondaryApiDataReader;
+    }
+
+    public async Task Run()
+    {
+        string? json = null;
+        try
+        {
+            json = await _apiDataReader.Read("https://swapi.dev/", "api/planets");
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("API request was unsuccessful. Switching to mock data. Exception message " + ex.Message);
+        }
+
+        // if json is null use mock class
+        if (json is null)
+        {
+            json = await _secondaryApiDataReader.Read("https://swapi.dev/", "api/planets");
+        }
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+
+    }
+}
+
+
