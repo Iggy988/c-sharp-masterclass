a01b496c9731dc7dfcdf2a5572d0600ce43541c6
Iggy988 <igor.rodic1988@gmail.com>
2023-10-21 22:20:41 +0200

    Type design

--- StarWarsPlanetStats/Program.cs
+++ StarWarsPlanetStats/Program.cs
@@ -49,6 +49,42 @@ internal class StarWarsPlanetsStatsApp
 
         var root = JsonSerializer.Deserialize<Root>(json);
 
+        var planets = ToPlanets(root);
+
+    }
+
+    private IEnumerable<Planet> ToPlanets(Root? root)
+    {
+        if (root is null)
+        {
+            throw new ArgumentNullException(nameof(root));
+        }
+        throw new NotImplementedException();
+    }
+}
+
+//immutable data
+public readonly record struct Planet
+{
+    public string Name { get; }
+    public int Diameter { get; }
+    public int? SurfaceWater { get; }
+    public int? Population { get; }
+
+    public Planet(
+        string name,
+        int diameter,
+        int? surfaceWater,
+        int? population)
+    {
+        if (name is null)
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+        Name = name;
+        Diameter = diameter;
+        SurfaceWater = surfaceWater;
+        Population = population;
     }
 }
 
