92d747989caae5102181b4e458b480036c767279
Iggy988 <igor.rodic1988@gmail.com>
2023-10-23 22:20:47 +0200

    Implementing IEnumerable

--- CollectionExample/Program.cs
+++ CollectionExample/Program.cs
@@ -36,6 +36,47 @@ public class CustomCollection : IEnumerable
 
     public IEnumerator GetEnumerator()
     {
-        throw new NotImplementedException();
+        return new WordEnumerator(Words);
+    }
+}
+
+public class WordEnumerator : IEnumerator
+{
+    //moramo staviti pocetnu poziciju na -1 jer kad budemo pozivali bice 0 na prvoj exekuciji metode MoveNext()
+    private const int InitialPosition = -1; 
+    private int _currentPosition = InitialPosition;
+    private readonly string[] _words;
+
+    public WordEnumerator(string[] words)
+    {
+        _words = words;
+    }
+
+    public object Current
+    {
+        get
+        {
+            try
+            {
+                return _words[_currentPosition];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+
+                throw new IndexOutOfRangeException($"{nameof(CustomCollection)}'s end reached. ", ex);
+            }
+            
+        }
+    }
+
+    public bool MoveNext()
+    {
+        ++_currentPosition;
+        return _currentPosition < _words.Length;
+    }
+
+    public void Reset()
+    {
+        _currentPosition = InitialPosition;
     }
 }
