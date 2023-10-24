68e201bb9061fa14513cd1293ad327d4cf2bc518
Iggy988 <igor.rodic1988@gmail.com>
2023-10-24 21:22:39 +0200

    Collection Initializers

--- CollectionExample/Program.cs
+++ CollectionExample/Program.cs
@@ -27,15 +27,33 @@ var enumerator = customCollection.GetEnumerator();
 //Indexer
 customCollection[1] = "abc";
 
+// da bi radilo moramo dodati parameterless constructor and Add method
+var newCollection = new CustomCollection
+{
+    "one", "two", "three"
+};
+
 Console.ReadKey();
 
 public class CustomCollection : IEnumerable<string>
 {
-    public string[] Words { get;  }
+    public string[] Words { get; }
 
     public CustomCollection(string[] words)
     {
         Words = words;
+    }
+
+    public CustomCollection()
+    {
+        Words = new string[10];
+    }
+
+    private int _currentIndex = 0;
+    public void Add(string word)
+    {
+        Words[_currentIndex] = word;  
+        ++_currentIndex;
     }
 
     //indexer
