0c2340a7c6227902533311b68a7e45ba7c198767
Iggy988 <igor.rodic1988@gmail.com>
2023-10-25 22:12:10 +0200

    Binary search Algorithm

--- CollectionExample2/Program.cs
+++ CollectionExample2/Program.cs
@@ -18,7 +18,16 @@ var readonlyDic = new ReadOnlyDictionary<string, int>(dictionary);
 //readonlyDic.Clear();
 //readonlyDic["bbb"] = 5;
 
+//binary search algorithm
+var sortedList = new List<int> { 1, 3, 4, 5, 6, 11, 12, 14, 16, 18 };
+var indexOf1 = sortedList.FindIndexInSorted(1);
+var indexof11 = sortedList.FindIndexInSorted(11);
+var indexOf12 = sortedList.FindIndexInSorted(12);
+var indexOf18 = sortedList.FindIndexInSorted(18);
+var indexOf13 = sortedList.FindIndexInSorted(13);
+
 Console.ReadKey();
+
 
 
 //Big O Notation
@@ -43,4 +52,33 @@ IEnumerable<string> ReadPlanets()
         "Pluto"
     };    
     return new ReadOnlyCollection<string>(result) ;  
+}
+
+public static class ListExtension
+{
+    //Binary search
+    public static int? FindIndexInSorted<T>(this IList<T> list, T itemToFind) where T : IComparable<T>
+    {
+        int leftBound = 0;
+        int rightBound = list.Count -1;
+
+        while(leftBound <= rightBound) 
+        {
+            int middleIndex = (leftBound + rightBound) / 2;
+            if (itemToFind.Equals(list[middleIndex]))
+            {
+                return middleIndex;
+            }
+            else if (itemToFind.CompareTo(list[middleIndex]) < 0)
+            {
+                rightBound = middleIndex - 1;
+            }
+            else
+            {
+                leftBound = middleIndex + 1;
+            }
+
+        }
+        return null;
+    }
 }
