bb528a53f81d48b3d8619e3f312967e07dbe16b1
Iggy988 <igor.rodic1988@gmail.com>
2023-10-29 19:57:00 +0100

    Custom Linked List Data Structure

--- CustomLinekdList/Program.cs
+++ CustomLinekdList/Program.cs
@@ -1,8 +1,79 @@
-﻿
+﻿using System.Collections;
 Console.ReadKey();
 
 public interface ILinkedList<T> : ICollection<T>
 {
     void AddToFront(T item);
     void AddToEnd(T item);
+}
+
+public class SinglyLinkedList<T> : ILinkedList<T?>
+{
+    private Node<T>? _head;
+    private int _count;
+
+    public int Count => _count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddToEnd(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddToFront(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T?[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T?> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Node<T>
+{
+    public T? Value { get; set; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T? value)
+    {
+            Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"Value: {Value}, Next: {(Next is null ? "null" : Next.Value)}";
+    }
 }
