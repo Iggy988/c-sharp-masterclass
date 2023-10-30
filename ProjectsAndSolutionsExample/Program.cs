209ac82944e00f88991d1ab6e54d8e8a1544abe5
Iggy988 <igor.rodic1988@gmail.com>
2023-10-30 21:33:06 +0100

    Projects and Assemblies

---
+++ ProjectsAndSolutionsExample/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Utilities;
+
+var numbers = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine(numbers.AsString());
+
+
+Console.WriteLine("Press any key to close app.");
+Console.ReadKey();
+
---
+++ Utilities/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Utilities;
+
+public static class EnumerableExtensions 
+{
+    public static string AsString<T>(this IEnumerable<T> items)
+    {
+        return string.Join(Environment.NewLine, items);
+    }
+}
