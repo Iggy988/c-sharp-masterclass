c13ccd1934950b7ab31682d38028e5ca3384f006
Iggy988 <igor.rodic1988@gmail.com>
2023-10-31 20:53:51 +0100

    internal

--- ProjectsAndSolutionsExample/Program.cs
+++ ProjectsAndSolutionsExample/Program.cs
@@ -5,6 +5,10 @@ var numbers = new int[] { 1, 2, 3, 4, 5 };
 Console.WriteLine(numbers.AsString());
 
 
+//InternalClass internalClass;
+//
+
+
 Console.WriteLine("Press any key to close app.");
 Console.ReadKey();
 
--- Utilities/EnumerableExtensions.cs
+++ Utilities/EnumerableExtensions.cs
@@ -7,3 +7,18 @@ public static class EnumerableExtensions
         return string.Join(Environment.NewLine, items);
     }
 }
+
+public class PublicClass
+{
+    private InternalClass _internalClass;
+    public int SomeMethod()
+    {
+        var someVariable =  new InternalClass();
+        return 0;
+    }
+}
+
+internal class InternalClass
+{
+    public static void PublishMethod(){ }
+}
