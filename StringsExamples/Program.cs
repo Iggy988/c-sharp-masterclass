11f1efa8f4777f49eeb3a71b788dd93d8fd64e05
Iggy988 <igor.rodic1988@gmail.com>
2023-11-02 21:51:08 +0100

    Strings are reference type

--- StringsExamples/Program.cs
+++ StringsExamples/Program.cs
@@ -8,5 +8,18 @@ var upperCase = text.ToUpper();
 Console.WriteLine(text);
 Console.WriteLine(upperCase);
 
+string text1 = "abc";
+string text2 = "abc";
+Console.WriteLine(text1.Equals(text2));
+
+Modify(text1);
+
+Console.WriteLine(text1);
 
 Console.ReadKey();
+
+
+void Modify(string input)
+{
+    input += "xyz";
+}
