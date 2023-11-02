722c9d0db85f183c1c435cb7f5ffa4a8c3f72345
Iggy988 <igor.rodic1988@gmail.com>
2023-11-02 21:29:59 +0100

    Chars

---
+++ CharsExample/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+char letter = 'a';
+char digit = '3';
+char symbol = '!';
+char newLine = '\n';
+
+var isUppercase = char.IsUpper(letter); //will be false
+var isLetter = char.IsLetter(digit); //will be false
+var isDigit = char.IsDigit(letter); //will be false
+var isWhitespace = char.IsWhiteSpace(newLine); //will be true
+var aToUpper = char.ToUpper(letter); //will give 'A'
+
+Console.ReadKey();
