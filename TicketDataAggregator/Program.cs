cde06b7702a458d3b33fcf186ce87fa72c28ba83
Iggy988 <igor.rodic1988@gmail.com>
2023-11-04 19:11:09 +0100

    Reading text from pdf

--- TicketDataAggregator/Program.cs
+++ TicketDataAggregator/Program.cs
@@ -1,3 +1,38 @@
-﻿
+﻿using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig;
+
+const string TicketFolder = @"D:\C#&.NET projects\C#Masteclass\TicketDataAggregator\Tickets";
+
+try
+{
+	var ticketsAggregator = new TicketsAggregator(TicketFolder);
+
+	ticketsAggregator.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An exception occured. Exception message: " + ex.Message );
+}
 
 Console.ReadKey();
+
+public class TicketsAggregator
+{
+    private string _ticketFolder;
+
+    public TicketsAggregator(string ticketFolder)
+    {
+        _ticketFolder = ticketFolder;
+    }
+
+    public void Run()
+    {
+        using (PdfDocument document = PdfDocument.Open(_ticketFolder + @"\Tickets1.pdf"))
+        {
+
+            Page page = document.GetPage(1);
+            string text = page.Text;
+        }
+    }
+}
+
