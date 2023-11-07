30b0fa56b9dd8944098dd277d771c06baad7b228
Iggy988 <igor.rodic1988@gmail.com>
2023-11-07 21:27:44 +0100

    Numeric overflow

--- NumericTypeExemple/Program.cs
+++ NumericTypeExemple/Program.cs
@@ -1,5 +1,19 @@
 ﻿
+var twoBillion = 2_000_000_000;
+var result = twoBillion + twoBillion;
 
+Console.WriteLine(result);
+
+int sumSoFar = 1_900_000_000;
+int nextTransaction = 1_000_000_000;
+if (sumSoFar + nextTransaction > twoBillion) //numeric overflow -> it will go negative
+{
+    Console.WriteLine("Transaction blocked.");
+}
+else
+{
+    Console.WriteLine("Transaction executed.");
+}
 
 
 Console.ReadKey();
