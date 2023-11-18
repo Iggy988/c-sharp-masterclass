c24fa99726c5f250135d693aec3dac20d1f922dc
Iggy988 <igor.rodic1988@gmail.com>
2023-11-18 19:12:08 +0100

    Advance mock setup

--- UnitTestExample/PersonalDataReaderTests.cs
+++ UnitTestExample/PersonalDataReaderTests.cs
@@ -13,12 +13,19 @@ public class PersonalDataReaderTests
         var databaseConnectionMock = new Mock<IDatabaseConnection>();
 
         databaseConnectionMock
-            .Setup(mock => mock.GetById(5))
+            //.SetupSequence(mock => mock.GetById(It.Is<int>(id => id > 0)))
+            //.Returns(new Person(1, "Ana", "Smith"))
+            //.Returns(new Person(2, "Jake", "Smith"))
+            //.Returns(new Person(3, "Bill", "Smith"));
+            .Setup(mock => mock.GetById(It.IsAny<int>()))
+            //.Throws(new Exception("Some message"));
             .Returns(new Person(5, "John", "Smith"));
 
         var personalDataReader = new PersonalDataReader(databaseConnectionMock.Object);
 
         string result = personalDataReader.Read(5);
+        //databaseConnectionMock.Reset();
+        //string result2 = personalDataReader.Read(5);
 
         Assert.AreEqual("(Id: 5) John Smith", result);
     }
